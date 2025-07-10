3eb3f99077f9d74de38acd150dc9176304a59895
Karima B <86795417+kb122215@users.noreply.github.com>
2025-07-10 01:30:53 -0400

    Commit 1
    First Commit

--- Trivia/TriviaQuestion.swift
+++ Trivia/TriviaQuestion.swift
@@ -7,9 +7,14 @@
 
 import Foundation
 
-struct TriviaQuestion {
+struct TriviaQuestion: Decodable {
   let category: String
   let question: String
   let correctAnswer: String
   let incorrectAnswers: [String]
+    enum CodingKeys: String, CodingKey {
+        case category, question
+        case correctAnswers = "correct_answer"
+        case incorrectAnswers = "incorrect_answers"
+    }
 }
